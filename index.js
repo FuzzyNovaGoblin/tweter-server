f38c9d9de6e20cd2e0dcb93ff5f73d56ad9e8b92
liam.cannon <liam.cannon@mymail.champlain.edu>
2021-04-09 16:52:02 -0400

    Index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express();
+const port = 8000;
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}!`)
+});
+// http://127.0.0.1:8000/
